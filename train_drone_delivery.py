d2f7580c8cece2cfcd9766537544abffe1a050e5
Enzo77360 <161731232+Enzo77360@users.noreply.github.com>
2024-12-02 15:05:55 +0100

    2

---
+++ train_drone_delivery.py
@@ -0,0 +1,27 @@
+import time
+from stable_baselines3 import PPO
+from drone_delivery_env import DroneDeliveryEnv
+
+# Créer un environnement simple (non vectorisé)
+env = DroneDeliveryEnv()
+
+# Créer un modèle PPO
+model = PPO("MlpPolicy", env, verbose=1)
+
+# Paramètre pour contrôler l'affichage pendant l'entraînement (True pour afficher, False pour ne pas afficher)
+show_render = False  # Modifiez ce paramètre selon votre souhait (True ou False)
+
+# Entraîner le modèle et afficher l'environnement
+total_timesteps = 10000
+n_display_interval = 1000  # Afficher l'environnement tous les 1000 timesteps
+
+for i in range(0, total_timesteps, n_display_interval):
+    model.learn(total_timesteps=n_display_interval, reset_num_timesteps=False)
+
+    # Afficher l'environnement après chaque intervalle d'entraînement si show_render est True
+    if show_render:
+        env.render()
+
+# Sauvegarder le modèle
+model.save("checkpoints/ppo_multi_drone_delivery_3")
+print("Entraînement terminé et modèle sauvegardé !")
